0080c4b3a10bd2281f60c0653e993049d55b8bce
Jeril-Albi <devjerilalbi@gmail.com>
2022-03-30 17:29:56 +0530

    init

---
+++ main.js
@@ -0,0 +1,20 @@
+//mobile menu
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navbar-nav");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("res");
+    navMenu.classList.toggle("res");
+    navLink.forEach(n => n.classList.toggle("res"));
+}
+//close menu
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu(){
+  hamburger.classList.remove("res");
+  navMenu.classList.remove("res");
+  navLink.forEach(n => n.classList.remove("res"));
+}
