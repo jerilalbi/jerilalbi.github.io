bf38a739ef4891438d89b905cc93473f4e7ad1f4
Jeril Albi <devjerilalbi@gmail.com>
2024-12-16 21:24:16 +0530

    project sec complete

--- version2/src/components/projectDetailsImg.jsx
+++ version2/src/components/projectDetailsImg.jsx
@@ -1,14 +1,117 @@
 import { Box } from "@mui/material";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ProjectContext } from "../providers/ProjectProvider";
 import { projectImgs } from "./image_helper";
 
 function ProjectDetailsImg() {
   const { openProject, projectImgSecRef } = useContext(ProjectContext);
+  const [ imgArrangement, setImagArrangement ] = useState([]);
+
+  function getValidCombinations(gridRows, gridCols, numBoxes) {
+    const gridArea = gridRows * gridCols;
+    const results = [];
+    const possibleBoxes = [
+      { rowspan: 1, colspan: 1 },
+      { rowspan: 1, colspan: 2 },
+      { rowspan: 1, colspan: 3 },
+      { rowspan: 2, colspan: 2 },
+      { rowspan: 2, colspan: 1 },
+      { rowspan: 3, colspan: 1 }
+  ];
+
+    function calculateArea(combination) {
+        return combination.reduce((sum, { rowspan, colspan }) => sum + rowspan * colspan, 0);
+    }
+
+    function fitsGrid(combination) {
+        const grid = Array.from({ length: gridRows }, () =>
+            Array(gridCols).fill(false)
+        );
+
+        let filledCells = 0;
+
+        for (const { rowspan, colspan } of combination) {
+            let placed = false;
+
+            for (let r = 0; r <= gridRows - rowspan; r++) {
+                for (let c = 0; c <= gridCols - colspan; c++) {
+                    let canPlace = true;
+                    for (let i = 0; i < rowspan; i++) {
+                        for (let j = 0; j < colspan; j++) {
+                            if (grid[r + i][c + j]) {
+                                canPlace = false;
+                                break;
+                            }
+                        }
+                        if (!canPlace) break;
+                    }
+
+                    if (canPlace) {
+                        for (let i = 0; i < rowspan; i++) {
+                            for (let j = 0; j < colspan; j++) {
+                                grid[r + i][c + j] = true;
+                            }
+                        }
+                        filledCells += rowspan * colspan;
+                        placed = true;
+                        break;
+                    }
+                }
+                if (placed) break;
+            }
+
+            if (!placed) return false;
+        }
+
+        return filledCells === gridArea;
+    }
+
+    function generateCombinations(possibleBoxes, numBoxes, start = 0, currentCombination = []) {
+        if (currentCombination.length === numBoxes) {
+            if (calculateArea(currentCombination) === gridArea && fitsGrid(currentCombination)) {
+                results.push([...currentCombination]);
+            }
+            return;
+        }
+
+        for (let i = start; i < possibleBoxes.length; i++) {
+            currentCombination.push(possibleBoxes[i]);
+            generateCombinations(possibleBoxes, numBoxes, i, currentCombination);
+            currentCombination.pop();
+        }
+    }
+
+    generateCombinations(possibleBoxes, numBoxes);
+    return results;
+}
+
+function getNoRows(imgLen){
+  if(imgLen > 0 && imgLen <= 6){
+    return 3;
+  }else if( imgLen > 6 && imgLen <= 8){
+    return 4;
+  }else if( imgLen > 8 && imgLen <= 10){
+    return 5;
+  }else{
+    return 6;
+  }
+}
+
 
   function randomNo(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
   }
+
+  useEffect(() => {
+    if(openProject){
+      const noRows = getNoRows(projectImgs.length);
+      const imgCombinations = getValidCombinations(noRows,3,projectImgs.length);
+      const arrangement = imgCombinations[randomNo(0,imgCombinations.length)];
+      setImagArrangement(arrangement)
+    }
+  },[openProject])
+
+  console.log("called ");
 
   return (
     <Box
@@ -32,12 +135,25 @@ function ProjectDetailsImg() {
           display: "grid",
           height: "100%",
           gridTemplateColumns: "1fr 1fr 1fr",
-          gridAutoRows: "200px",
+          gridAutoRows: "207px",
           overflowY: "auto",
           gap: "5px",
+          gridAutoFlow: "dense",
         }}
       >
-        {projectImgs.map((img) => (
+        { imgArrangement.length > 0 && projectImgs.map((img, index) => (
+          <Box
+            sx={{
+              // gridRow: `span 2`,
+              gridRow: `span ${imgArrangement[index].rowspan}`,
+              gridColumn: `span ${imgArrangement[index].colspan}`,
+              borderRadius: "15px",
+              background: `url(${img})`,
+              backgroundSize: "cover",
+            }}
+          />
+        ))}
+        {/* {projectImgs.map((img) => (
           <Box
             sx={{
               // gridRow: `span 2`,
@@ -48,7 +164,7 @@ function ProjectDetailsImg() {
               backgroundSize: "cover",
             }}
           />
-        ))}
+        ))} */}
       </Box>
     </Box>
   );
--- version2/src/components/projectDetailsText.jsx
+++ version2/src/components/projectDetailsText.jsx
@@ -1,7 +1,8 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React, { useContext, useEffect } from "react";
 import { ProjectContext } from "../providers/ProjectProvider";
 import CloseIcon from "@mui/icons-material/Close";
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 function ProjectDetailsText() {
   const { openProject, setOpenProject, projectImgSecRef, projectTextSecRef } =
@@ -65,15 +66,61 @@ function ProjectDetailsText() {
       <Box
         ref={projectTextSecRef}
         sx={{
-          height: "100%",
+          display: 'flex',
+          flexDirection: 'column',
+          height: "94.9%",
           width: "100%",
           bgcolor: "primary.main",
+          color: "white",
+          textAlign: "start",
           borderRadius: "0px 15px 15px 0",
           marginTop: "13px",
-          padding: "8px",
-          boxSizing: "border-box",
+          paddingY: "10px",
+          paddingX: "20px",
+          boxSizing: "border-box"
         }}
-      ></Box>
+      >
+        <Box sx={{
+          display: 'inline', 
+          alignItems: 'baseline',
+          }}>
+          <Typography
+          component={'span'}
+            sx={{
+              fontSize: "30px",
+              fontWeight: "750",
+              fontFamily: "Lato, sans-serif",
+            }}
+          >
+            Cafe management sytem
+          </Typography>
+          <GitHubIcon sx={{height: '30px', width: "30px", marginLeft: '10px',cursor: 'pointer'}}/>
+        </Box>
+        <Typography
+        sx={{
+          fontWeight: "750",
+          whiteSpace: "pre",
+          marginTop: '15px'
+        }}
+        >
+          {`FLUTTER | FIREBASE | BLOC`}
+        </Typography>
+        <Box sx={{
+          flex: 1,
+          overflowY: "auto",
+          marginTop: '15px',
+        }}>
+        <Typography
+        sx={{
+          fontWeight: "500",
+          textAlign: 'justify',
+        }}>
+          Lorem Ipsum is simple dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown writer took a handful of words and scrambled them to make a type specimen book. Not only has it survived five centuries, but it has also made it into electronic typesetting (note, almost unchanged). It was popularised in the 1960s with the release of "Letraset", which contained passages of Lorem Ipsum, as did desktop software like "Aldus PageMaker", also using Lorem Ipsum.
+          Lorem Ipsum is simple dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown writer took a handful of words and scrambled them to make a type specimen book. Not only has it survived five centuries, but it has also made it into electronic typesetting (note, almost unchanged). It was popularised in the 1960s with the release of "Letraset", which contained passages of Lorem Ipsum, as did desktop software like "Aldus PageMaker", also using Lorem Ipsum.
+          Lorem Ipsum is simple dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown writer took a handful of words and scrambled them to make a type specimen book. Not only has it survived five centuries, but it has also made it into electronic typesetting (note, almost unchanged). It was popularised in the 1960s with the release of "Letraset", which contained passages of Lorem Ipsum, as did desktop software like "Aldus PageMaker", also using Lorem Ipsum.
+        </Typography>
+        </Box>
+      </Box>
     </Box>
   );
 }
